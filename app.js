b662e2265095fed36c13d1f32fd7c447c158f271
hazelisles <zhccj19@gmail.com>
2021-05-22 15:49:06 +0800

    feat: add measure function

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const getDurationInMilliseconds = (start) => {
+    const NS_PER_SEC = 1e9
+    const NS_TO_MS = 1e6
+    const diff = process.hrtime(start)
+
+    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
+}
+
 app.use((req, res, next) => {
+  const start = process.hrtime()
   const nDate = new Date()
   const year = nDate.getFullYear()
   const month = ("0" + (nDate.getMonth() + 1)).slice(-2)
@@ -10,8 +19,15 @@ app.use((req, res, next) => {
   const hour = ("0" + nDate.getHours()).slice(-2)
   const min = ("0" + nDate.getMinutes()).slice(-2)
   const sec = ("0" + nDate.getSeconds()).slice(-2)
-  const meg = `${year}-${month}-${date} ${hour}:${min}:${sec} | ${req.method} from ${req.url}`
-  console.log(meg)
+  const meg1 = `${year}-${month}-${date} ${hour}:${min}:${sec} | ${req.method} from ${req.url}`
+  console.log(meg1)
+
+  res.on('finish', () => {
+    const duration = getDurationInMilliseconds(start)
+    const meg2 = `${year}-${month}-${date} ${hour}:${min}:${sec} | ${req.method} from ${req.url} | total time: ${duration}ms`
+    console.log(meg2)
+  })
+  
   next()
 })
 
